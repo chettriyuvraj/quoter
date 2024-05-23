6e2475cc0931cf26ba7671de65353f5d9f3a36a7
Yuvraj Chettri <chettriyuvraj@gmail.com>
2024-05-23 19:56:09 +0530

    1. feat: add parsing logic for quote sub-command 2. test: add parsing logic test for quote sub-command

--- cmd/quote.go
+++ cmd/quote.go
@@ -1,1 +1,39 @@
 package cmd
+
+import (
+	"flag"
+	"fmt"
+	"io"
+)
+
+type QuoteConfig struct {
+	Genre string `json:"genre"`
+}
+
+const (
+	QUOTE_USAGE_STRING = `
+quote: returns a quote from stored list
+
+usage: quote` /* TODO: Add proper usage */
+)
+
+func parseQuoteArgs(w io.Writer, args []string) (QuoteConfig, error) {
+	var config QuoteConfig
+
+	fs := flag.NewFlagSet("quote", flag.ContinueOnError)
+	fs.StringVar(&config.Genre, "g", "", "genre from which we want a quote")
+
+	fs.SetOutput(w)
+	fs.Usage = func() {
+		fmt.Fprint(w, QUOTE_USAGE_STRING)
+		fmt.Fprintln(w)
+		fs.PrintDefaults()
+	}
+
+	err := fs.Parse(args)
+	if err != nil {
+		return config, err
+	}
+
+	return config, nil
+}
--- cmd/quote_test.go
+++ cmd/quote_test.go
@@ -1,1 +1,62 @@
 package cmd
+
+import (
+	"bytes"
+	"flag"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestParseQuoteArgs(t *testing.T) {
+
+	tcs := []struct {
+		desc   string
+		args   []string
+		err    error
+		want   QuoteConfig
+		output string
+	}{
+		{
+			desc:   "help flag",
+			args:   []string{"-h"},
+			err:    flag.ErrHelp,
+			output: QUOTE_USAGE_STRING,
+		},
+		{
+			desc:   "non existent flag",
+			args:   []string{"-x"},
+			err:    flag.ErrHelp,
+			output: QUOTE_USAGE_STRING,
+		},
+		{
+			desc: "genre flag only",
+			args: []string{"-g", "romance"},
+			want: QuoteConfig{Genre: "romance"},
+		},
+		{
+			desc:   "genre flag but no genre specifed",
+			args:   []string{"-g"},
+			err:    flag.ErrHelp,
+			output: QUOTE_USAGE_STRING,
+		},
+		{
+			desc: "no flags",
+			args: []string{""},
+			want: QuoteConfig{Genre: ""},
+		},
+	}
+
+	for _, tc := range tcs {
+		buf := bytes.Buffer{}
+		got, err := parseQuoteArgs(&buf, tc.args)
+		if tc.err != nil {
+			require.Error(t, tc.err, err, tc.desc)
+			continue
+		}
+		require.NoError(t, err, tc.desc)
+		require.Equal(t, tc.want, got, tc.desc)
+		require.Equal(t, tc.output, buf.String(), tc.desc)
+
+	}
+}
