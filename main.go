ac17c9ffe41398caba1f63aa6d0f06eb54a298d2
Yuvraj Chettri <chettriyuvraj@gmail.com>
2024-05-24 01:47:55 +0530

    feat: add main function

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/chettriyuvraj/quoter/cmd"
+)
+
+func main() {
+	switch os.Args[1] {
+	case "add":
+		err := cmd.HandleAdd(os.Stdout, os.Args[2:])
+		if err != nil {
+			if errors.Is(err, cmd.ErrNoQuotesFound) {
+				fmt.Fprint(os.Stderr, err.Error())
+				fmt.Fprintln(os.Stderr)
+			}
+			printUsage()
+		}
+	case "quote":
+		err := cmd.HandleQuote(os.Stdout, os.Args[2:])
+		if err != nil {
+			if errors.Is(err, cmd.ErrNoQuotesFound) || errors.Is(err, cmd.ErrNoGenreSpecificQuotesFound) {
+				fmt.Fprint(os.Stderr, err.Error())
+				fmt.Fprintln(os.Stderr)
+				return
+			}
+			printUsage()
+		}
+	}
+}
+
+func printUsage() {
+	usageString := `
+usage: %s <add|quote> [-g genre] [quote]`
+	fmt.Fprint(os.Stderr, usageString)
+	fmt.Fprintln(os.Stderr)
+	fmt.Fprint(os.Stderr, cmd.ADD_USAGE_STRING)
+	fmt.Fprintln(os.Stderr)
+	fmt.Fprint(os.Stderr, cmd.QUOTE_USAGE_STRING)
+}
