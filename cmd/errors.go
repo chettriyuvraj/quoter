3eb95d93a3998f95812eee9c8c37807d263550b2
Yuvraj Chettri <chettriyuvraj@gmail.com>
2024-05-27 00:22:02 +0530

    doc: minor change to error message

--- cmd/errors.go
+++ cmd/errors.go
@@ -2,6 +2,6 @@ package cmd
 
 import "errors"
 
-var ErrNoPositionalArgs = errors.New("no positional arg provided")
+var ErrNoPositionalArgs = errors.New("no positional args provided")
 var ErrNoQuotesFound = errors.New("no quotes found in database")
 var ErrNoGenreSpecificQuotesFound = errors.New("no quotes found for specified genre")
