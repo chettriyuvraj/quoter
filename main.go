1f32d4af954f12af3b0f90a3c89a332933bbf164
Yuvraj Chettri <chettriyuvraj@gmail.com>
2024-05-26 22:52:16 +0530

    feat: add driver code in main

--- main.go
+++ main.go
@@ -2,55 +2,74 @@ package main
 
 import (
 	"errors"
+	"flag"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/chettriyuvraj/quoter/cmd"
 )
 
-var ErrInternal = errors.New("internal error")
 var ErrInvalidSubCmd = errors.New("invalid subcommand")
-var usageString = `
-Usage: %s <COMMAND> [OPTIONS]`
+var ErrNoPositionalArgs = errors.New("no positional args provided")
+
+const (
+	USAGE_STRING = `
+Usage: quoter <COMMAND> [OPTIONS]` /* TODO add dynamic command name */
+)
 
 func main() {
-	handleCmd(os.Args)
+	err := runCmd(os.Stderr, os.Args[1:])
+	if err != nil {
+		os.Exit(1)
+	}
 }
 
-func handleCmd(args []string) {
+/* No abstractions here - parsing and running simultaneously */
+func runCmd(w io.Writer, args []string) error {
 	var err error
 
-	switch os.Args[1] {
+	fs := flag.NewFlagSet("quoter", flag.ContinueOnError)
+	fs.SetOutput(w)
+	fs.Usage = func() {
+		printUsage(w)
+	}
+
+	/* Parse */
+	err = fs.Parse(args)
+	if err != nil {
+		return err
+	}
+
+	/* First positional arg treated as sub-command */
+	if fs.NArg() == 0 {
+		fmt.Fprint(w, ErrNoPositionalArgs.Error())
+		fmt.Fprintln(w)
+		printUsage(w)
+		return ErrNoPositionalArgs
+	}
+	switch fs.Arg(0) {
 	case "add":
-		err = cmd.HandleAdd(os.Stdout, os.Args[2:])
+		err = cmd.HandleAdd(w, args[1:])
 	case "quote":
-		err = cmd.HandleQuote(os.Stdout, os.Args[2:])
-	case "-h":
-		printUsage(os.Args[0])
-	case "--help":
-		printUsage(os.Args[0])
+		err = cmd.HandleQuote(w, args[1:])
 	default:
+		fmt.Fprint(w, ErrInvalidSubCmd.Error())
+		fmt.Fprintln(w)
+		printUsage(w)
 		err = ErrInvalidSubCmd
 	}
 
-	if err != nil {
-		/* If not a known error, mark as internal error - don't expose */
-		if !errors.Is(err, cmd.ErrNoQuotesFound) || !errors.Is(err, cmd.ErrNoGenreSpecificQuotesFound) {
-			err = ErrInternal
-		}
-		fmt.Fprint(os.Stderr, "Error: ")
-		fmt.Fprint(os.Stderr, err.Error())
-		fmt.Fprintln(os.Stderr)
-		printUsage(os.Args[0])
-	}
+	return err
+
 }
 
-func printUsage(cmdName string) {
-	fmt.Fprintf(os.Stderr, usageString, cmdName)
-	fmt.Fprintln(os.Stderr)
-	fmt.Fprintln(os.Stderr)
-	fmt.Fprint(os.Stderr, "COMMANDS:")
-	fmt.Fprintln(os.Stderr)
-	cmd.HandleAdd(os.Stdout, []string{"-h"})
-	cmd.HandleQuote(os.Stdout, []string{"-h"})
+func printUsage(w io.Writer) {
+	fmt.Fprint(w, USAGE_STRING)
+	fmt.Fprintln(w)
+	fmt.Fprintln(w)
+	fmt.Fprint(w, "COMMANDS:")
+	fmt.Fprintln(w)
+	cmd.HandleAdd(w, []string{"-h"})
+	cmd.HandleQuote(w, []string{"-h"})
 }
